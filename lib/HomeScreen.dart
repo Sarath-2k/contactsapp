3608a6a1d42d35492bff102e187deb7fed15ab5f
sonu-simon <sonuisplaying@gmail.com>
2020-06-15 19:51:12 +0530

    search beta

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -11,9 +11,45 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController editingController = TextEditingController();
+  final List<Contact> filteredList = List<Contact>();
+
+  List<Contact> tempList = List<Contact>();
+  copyListData(List<Contact> copiedData) {
+    tempList.clear();
+    filteredList.clear();
+    tempList.addAll(copiedData);
+    filteredList.addAll(tempList);
+  }
+
+  void filterSearch(String query) {
+    List<Contact> dummySearchList = List<Contact>();
+    dummySearchList.addAll(tempList);
+
+    if (query.isNotEmpty) {
+      List<Contact> tempListWithData = List<Contact>();
+      dummySearchList.forEach((element) {
+        if (element.name.contains(query)) {
+          tempListWithData.add(element);
+        }
+      });
+      setState(() {
+        filteredList.clear();
+        filteredList.addAll(tempListWithData);
+      });
+      return;
+    } else {
+      setState(() {
+        filteredList.clear();
+        filteredList.addAll(tempList);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final contactssnap = Provider.of<List<Contact>>(context) ?? [];
+    copyListData(contactssnap);
 
     return StreamProvider<List<Contact>>.value(
       value: DatabaseService().contactssnap,
@@ -21,33 +57,53 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           title: Center(child: Text("ContactsApp")),
         ),
-        body: ListView.builder(
-          itemBuilder: (context, index) {
-            return Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Material(
-                elevation: 2,
-                child: ListTile(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) =>
-                                ContactDetails(contactssnap[index])));
-                  },
-                  leading: CircleAvatar(
-                    radius: 20,
-                    backgroundImage: contactssnap[index].imageurl != ""
-                          ? NetworkImage(contactssnap[index].imageurl)
-                          : AssetImage("images/profile.jpg"),
-                  ),
-                  title: Text(contactssnap[index].name),
-                  subtitle: Text(contactssnap[index].fathername ?? ""),
-                ),
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: TextField(
+                onChanged: (value) {
+                  filterSearch(value);
+                },
+                controller: editingController,
+                decoration: InputDecoration(
+                    labelText: "Search",
+                    hintText: "Search",
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
               ),
-            );
-          },
-          itemCount: contactssnap.length,
+            ),
+            ListView.builder(
+              shrinkWrap: true,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Material(
+                    elevation: 2,
+                    child: ListTile(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    ContactDetails(filteredList[index])));
+                      },
+                      leading: CircleAvatar(
+                        radius: 20,
+                        backgroundImage: filteredList[index].imageurl != ""
+                            ? NetworkImage(filteredList[index].imageurl)
+                            : AssetImage("images/profile.jpg"),
+                      ),
+                      title: Text(filteredList[index].name),
+                      subtitle: Text(filteredList[index].fathername ?? ""),
+                    ),
+                  ),
+                );
+              },
+              itemCount: filteredList.length,
+            ),
+          ],
         ),
       ),
     );
